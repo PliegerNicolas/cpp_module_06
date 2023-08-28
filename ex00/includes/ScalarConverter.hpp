0f7674ffbbf411a422d91312d56967b5003dde1a
Nicolas PLIEGER <plieger05@gmail.com>
2023-08-28 17:59:26 +0200

    Initial commit

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 17:34:27 by nplieger          #+#    #+#             */
+/*   Updated: 2023/08/28 17:59:01 by nplieger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+
+# include <iostream>
+# include <string>
+
+class	ScalarConverter
+{
+	private:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	protected:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	public:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+};
